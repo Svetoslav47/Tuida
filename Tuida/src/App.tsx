e86b15ba599f9f46e732e81dd58e870d92a21711
Svetoslav47 <svetoslav2806@gmail.com>
2025-06-14 20:32:35 +0300

    remove usless import

--- Tuida/src/App.tsx
+++ Tuida/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Layout from './components/Layout';
 import YourHome from './pages/YourHome';
 import Us from './pages/Us';
